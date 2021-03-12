29e90f5861acc4c0508a6e8c53ed1e632e8e56f3
aaronwang <aaronwang@tencent.com>
2021-03-12 10:06:24 +0800

    【Fluid】新增统一头文件 【PBFDemo】新增预依赖头文件；新增依赖库Platform

--- code/PBFDemo/main.cpp
+++ code/PBFDemo/main.cpp
@@ -1,9 +1,13 @@
 ﻿
 
-#include <stdio.h>
+#include "prerequisites.h"
 
 
 int main(int argc, char* argv[])
 {
+	Tiny3D::System* system = new Tiny3D::System();
+
+
+	delete system;
 	return 0;
 }
---
+++ code/PBFDemo/prerequisites.h
@@ -0,0 +1,10 @@
+﻿
+
+#ifndef __PREREQUITISTES_H__
+#define __PREREQUITISTES_H__
+
+
+#include <Fluid.h>
+
+
+#endif  /*__PREREQUITISTES_H__*/
